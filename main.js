aef483f236427a600a278da7a45b1da4a4cae6f2
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-03-06 16:27:41 -0500

    TP1 VERSION FINALE

---
+++ main.js
@@ -0,0 +1,5 @@
+// Extra javascript que j'ai rajouté pour servir à une icône de FontAwesome
+document.getElementById("btn-imprimer").addEventListener("click", (event) => {
+  event.preventDefault();
+  print();
+});
