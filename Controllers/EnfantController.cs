6d9aca9e62c3ad1c741427a325fcbc4fef24ae93
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-03-17 18:41:02 -0400

    2024-03-17
    Commencement du TP2
    (1re étape faite)

---
+++ Controllers/EnfantController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_WEB.Controllers
+{
+    public class EnfantController : Controller
+    {
+        public IActionResult Recherche()
+        {
+            return View();
+        }  
+        
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
--- main.js
+++
@@ -1,5 +0,0 @@
-// Extra javascript que j'ai rajouté pour servir à une icône de FontAwesome
-document.getElementById("btn-imprimer").addEventListener("click", (event) => {
-  event.preventDefault();
-  print();
-});
